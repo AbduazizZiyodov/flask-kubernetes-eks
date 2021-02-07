2da23f206eb38e22f564e53dd1ac430d615b4148
AbduazizZiyodov <abduaziz.ziyodov@mail.ru>
2021-02-07 20:22:44 +0500

    🖥 Update URL

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-export HOST=""
+export HOST="3.133.98.33"
 export TOKEN=`curl -d '{"email":"test@gmail.com","password":"password"}' -H "Content-Type: application/json" -X POST ${HOST}/auth  | jq -r '.token'`
-curl --request GET ${URL}:80/contents -H "Authorization: Bearer ${TOKEN}" | jq
+curl --request GET ${HOST}:80/contents -H "Authorization: Bearer ${TOKEN}" | jq
